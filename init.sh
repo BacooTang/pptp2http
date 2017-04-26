3ac67caa59b5d1be19f156369ee1137277b87830
bacootang <bacootang@gmail.com>
2017-04-27 06:27:37 +0800

    最新动态获取代理

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ chmod 755 /etc/ppp/ip-up.d/000ppp0
 pptpsetup --create pptp --server "$1" --username "$2" --password "$3"
 # exec pppd call pptp persist nodetach debug &
 pon pptp persist
-nodejs /proxy.js
+# nodejs /proxy.js
+curl baidu.com
