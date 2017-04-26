c52cfa05b1611ab5db903722112f516d6e173022
bacootang <bacootang@gmail.com>
2017-04-27 05:42:54 +0800

    最新动态获取代理

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ umask 022
 mkdir -p /data
 chmod 755 /etc/ppp/ip-up.d/000ppp0
 pptpsetup --create pptp --server "$1" --username "$2" --password "$3"
-exec pppd call pptp usepeerdns persist nodetach debug &
+exec pppd call pptp persist nodetach debug &
 #pon pptp persist
 nodejs /proxy.js
