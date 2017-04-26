59e8895f77971cd66e21cb5ca2e5236094f40212
bacootang <bacootang@gmail.com>
2017-04-27 06:01:00 +0800

    最新动态获取代理

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ pptpsetup --create pptp --server "$1" --username "$2" --password "$3"
 # exec pppd call pptp persist nodetach debug &
 pon pptp persist
 # nodejs /proxy.js
-ping baidu.com
+ping fucku.sh.1251900689.clb.myqcloud.com
