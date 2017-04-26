ebd5cc4b5192f2b446058d7997905959100d6249
bacootang <bacootang@gmail.com>
2017-04-27 02:04:17 +0800

    最新动态获取代理

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 chmod 755 /etc/ppp/ip-up.d/000ppp0
-pptpsetup --create pptp --server "$1" --username "$2" --password "$3"
+pptpsetup --create pptp --server "$1" --username "$2" --password "$3" --start &
 # pon pptp persist
-nodejs /proxy.js
+# nodejs /proxy.js
+
+sleep 10
+curl 'http://fucku.sh.1251900689.clb.myqcloud.com/ip'
